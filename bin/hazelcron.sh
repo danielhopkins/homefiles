338c628ea483e980fa803caf39e6d607e1072eb8
Dan Hopkins <dhopkins@ziaconsulting.com>
2011-03-24 07:45:35 -0600

    new script to manage dropbox

--- bin/hazelcron.sh
+++ bin/hazelcron.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+DROPBOX_FOLDER='/Users/dhopkins/Dropbox'
+LOG_FILE='/var/log/hazelcron.log'
+{
+    echo $(date) 
+    # Stop if there is no Dropbox folder. If it exists then do do some setup of the special folders, if they have been removed.
+    [ ! -e $DROPBOX_FOLDER ] && { echo 'no dropbox'; exit 1; } 
+    [ ! -e $DROPBOX_FOLDER/Public/week ] && mkdir -v $DROPBOX_FOLDER/Public/week
+    [ ! -e $DROPBOX_FOLDER/Public/hour ] && mkdir -v $DROPBOX_FOLDER/Public/hour
 
-# Stop if there is no Dropbox folder. If it exists then do do some setup of the special folders, if they have been removed.
-[ ! -e ~/Dropbox ] && { echo 'no dropboxi'; exit 1; } 
-[ ! -e ~/Dropbox/Public/week ] && mkdir ~/Dropbox/Public/week
-[ ! -e ~/Dropbox/Public/hour ] && mkdir ~/Dropbox/Public/hour
+    find $DROPBOX_FOLDER/Public/week/ -type f -mtime +7d -exec rm -v {} \;
+    find $DROPBOX_FOLDER/Public/hour/ -type f -mtime +1h -exec rm -v {} \;
 
-find ~/Dropbox/Public/week/ -mtime +7d -exec rm {} n removed.;
-find ~/Dropbox/Public/hour/ -mtime +1h -exec rm {} \;
-
-# Move any extra files into the perm area
-find ~/Dropbox/Public/ -type f -depth 1 -and -not -name '*Icon*' -and -not -name '.*' -exec mv {} ~/Dropbox/Public/perm \;
+    # Move any extra files into the perm area
+    find $DROPBOX_FOLDER/Public -type f -depth 1 -and -not -name '*Icon*' -and -not -name '.*' -exec mv -v {} $DROPBOX_FOLDER/Public/perm \;
+} >> $LOG_FILE
